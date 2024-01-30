9b050189ab1db057193409751aa876d07cdfefab
cwj2099 <wc1629@nyu.edu>
2024-01-30 18:19:31 -0500

    testing with fake occulision

---
+++ Assets/WujiFolder/Mask.shader
@@ -0,0 +1,14 @@
+﻿Shader "Custom/Mask"
+{
+
+  SubShader
+  {
+	 Tags {"Queue" = "Transparent+1"}	 
+
+  Pass
+     {
+		 Blend Zero One 
+     }
+  }
+
+}
---
+++ Assets/WujiFolder/MaskObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskObject : MonoBehaviour
+{
+    public GameObject[] ObjMasked;
+    void Update()
+    {
+        for (int i = 0; i < ObjMasked.Length; i++)
+        {
+            ObjMasked[i].GetComponent<MeshRenderer>().material.renderQueue = 3002;
+            foreach(MeshRenderer meshRenderer in ObjMasked[i].GetComponentsInChildren<MeshRenderer>())
+            {
+                meshRenderer.material.renderQueue = 3002;
+            }
+            foreach(SkinnedMeshRenderer skinnedMeshRenderer in ObjMasked[i].GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                foreach(Material m in skinnedMeshRenderer.materials) { m.renderQueue = 3002; }
+            }
+        }
+    }
+}
+
