1eab51ab8339ccaba63b6cb97f84c24cd91e2cb3
cwj2099 <wc1629@nyu.edu>
2024-02-28 17:09:27 -0500

    better looking

--- Assets/Scripts/DoTweenFeedback.cs
+++ Assets/Scripts/DoTweenFeedback.cs
@@ -2,16 +2,30 @@ using DG.Tweening;
 using UnityEngine;
 
 public class DoTweenFeedback : MonoBehaviour {
-   public float shakeStrength = 1.0f;
-   protected Tween _shakeTween;
+    public bool rotating = false;
+    public float rotationSpeed = 60f;
+    public float shakeStrength = 1.0f;
+    protected Tween _shakeTween;
 
-   public void Shake() {
-      Shake(0.5f);
-   }
+    public void Shake()
+    {
+        Shake(0.5f);
+    }
 
-   public void Shake(float t) {
-      if (_shakeTween == null || !_shakeTween.active) {
-         _shakeTween = transform.DOShakeScale(t, shakeStrength, 200, 90, false, ShakeRandomnessMode.Harmonic);
-      }
-   }
+    public void Shake(float t)
+    {
+        if (_shakeTween == null || !_shakeTween.active)
+        {
+            _shakeTween = transform.DOShakeScale(t, shakeStrength, 200, 90, false, ShakeRandomnessMode.Harmonic);
+        }
+    }
+
+    private void Update()
+    {
+        if(rotating)
+        {
+            transform.Rotate(new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime, Space.Self);
+        }
+    }
+
 }
