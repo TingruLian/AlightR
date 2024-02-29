50d0b77dcc6a0df954ad753b84e8457715aaa5f8
Dmitry Portnoy <dportnoy@andrew.cmu.edu>
2024-02-28 19:12:46 -0500

    Fix the attribute class and change the turrets to flying turrets, so they slow enemies down

--- Assets/Scripts/Attribute.cs
+++ Assets/Scripts/Attribute.cs
@@ -3,11 +3,12 @@ public class Attribute<T> {
    private T curValue;
 
    public T GetBaseValue() {
-      return curValue;
+      return baseValue;
    }
 
    public void SetBaseValue(T val) {
       baseValue = val;
+      curValue = val; // may not be the right approach if we allow changing the base value after creation
    }
 
    public T GetCurValue() {
