ff06d1e9a72e9ee870a71fb8336bb342c23640ef
cwj2099 <wc1629@nyu.edu>
2024-03-12 14:16:55 -0400

    some map set up

--- Assets/WujiFolder/Script/Map/LocationSpawnControl.cs
+++ Assets/WujiFolder/Script/Map/LocationSpawnControl.cs
@@ -10,33 +10,39 @@ using TMPro;
 [Serializable]
 public struct LocationUnit
 {
-    public SerializableLatLng position;
-    public string name;
-    public int sceneID;
-    public Sprite sprite;
+   public SerializableLatLng position;
+   public string name;
+   public int sceneID;
+   public Sprite sprite;
+   public GameObject child;
 }
 
 public class LocationSpawnControl : MonoBehaviour
 {
-    [SerializeField] private LayerGameObjectPlacement _layerObject;
-    public List<LocationUnit> units;
+   [SerializeField] private LayerGameObjectPlacement _layerObject;
+   public List<LocationUnit> units;
 
-    private void Awake()
-    {
-        if(_layerObject == null) _layerObject = GetComponent<LayerGameObjectPlacement>();
-    }
+   private void Awake()
+   {
+      if (_layerObject == null) _layerObject = GetComponent<LayerGameObjectPlacement>();
+   }
 
 
-    private void Start()
-    {
-        foreach(LocationUnit unit in units)
-        {
-            GameObject location = _layerObject.PlaceInstance(unit.position, unit.name).Value;
-            location.GetComponentInChildren<TextMeshProUGUI>().text = unit.name;
-            location.GetComponent<SceneLoader>().SceneId = unit.sceneID;
-            location.GetComponentInChildren<Image>().sprite = unit.sprite;
-        }
-    }
+   private void Start()
+   {
+      foreach (LocationUnit unit in units)
+      {
+         GameObject location = _layerObject.PlaceInstance(unit.position, unit.name).Value;
+         location.GetComponentInChildren<TextMeshProUGUI>().text = unit.name;
+         location.GetComponent<SceneLoader>().SceneId = unit.sceneID;
+         location.GetComponentInChildren<Image>().sprite = unit.sprite;
+
+         if(unit.child != null)
+         {
+            Instantiate(unit.child,location.transform.position, Quaternion.identity);
+         }
+      }
+   }
 
 
 }
