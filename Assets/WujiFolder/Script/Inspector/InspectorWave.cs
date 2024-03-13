e01f24dab6b6e620077f3e4eec51eccb74692a4c
cwj2099 <wc1629@nyu.edu>
2024-03-13 16:27:11 -0400

    bug fix

--- Assets/WujiFolder/Script/Inspector/InspectorWave.cs
+++ Assets/WujiFolder/Script/Inspector/InspectorWave.cs
@@ -1,3 +1,4 @@
+#if (UNITY_EDITOR) 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,3 +29,4 @@ public class InspectorWave : Editor
       DrawDefaultInspector();
    }
 }
+#endif
