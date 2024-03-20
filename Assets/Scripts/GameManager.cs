e341d24023778d4c6a910f93ae4c99808b3ee25f
cwj2099 <wc1629@nyu.edu>
2024-03-20 17:35:05 -0400

    rotation tested works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.Playables;
-using UnityEditor.ShaderGraph;
 using UnityEditor;
 
 public class GameManager : MonoBehaviour {
