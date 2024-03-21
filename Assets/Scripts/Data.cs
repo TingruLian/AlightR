8b5fe0f5caf9c1ea26dd24a5e4eba3eb5b4b6955
cwj2099 <wc1629@nyu.edu>
2024-03-21 15:39:05 -0400

    moved data position to Resources folder

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -2,15 +2,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GameScriptableObject", order = 1)]
-public class GameScriptableObject : ScriptableObject {
+public class GameScriptableObject : ScriptableObject
+{
 
-    public int grassCount;
-    public int bookHP;
-    public List<bool> completionState;
-    public void OnEnable() {
-        grassCount = 0;
-        bookHP = 10;
-    }
+   public int grassCount;
+   public int bookHP;
+   public List<bool> completionState;
+   public List<bool> tutorialCompletion;
+   public void OnEnable()
+   {
+      grassCount = 0;
+      bookHP = 10;
+   }
 }
 
 
