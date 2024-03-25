497f9d6d57cd79a5c205a537c1e91dc6990eb94b
Dmitry Portnoy <dportnoy@andrew.cmu.edu>
2024-03-25 19:27:31 -0400

    Copy the map scene, and a metal cart that can move across the bridge

--- Assets/WujiFolder/Script/Map/LocationSpawnControl.cs
+++ Assets/WujiFolder/Script/Map/LocationSpawnControl.cs
@@ -1,11 +1,8 @@
 using Niantic.Lightship.Maps.MapLayers.Components;
 using Niantic.Lightship.Maps.Coordinates;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 [Serializable]
 public struct LocationUnit
@@ -20,32 +17,77 @@ public struct LocationUnit
 
 public class LocationSpawnControl : MonoBehaviour
 {
-   [SerializeField] private LayerGameObjectPlacement _layerObject;
-   public List<LocationUnit> units;
-    [SerializeField] private GameScriptableObject GameData;
+   [SerializeField]
+   private LayerGameObjectPlacement _layerObject;
 
-   private void Awake()
-   {
+   public List<LocationUnit> units;
+
+   [SerializeField]
+   private GameScriptableObject GameData;
+
+   [SerializeField]
+   private LayerGameObjectPlacement cart;
+
+   [SerializeField]
+   private SerializableLatLng cartPosition;
+
+   private Vector2 cartStart;
+   private Vector2 cartEnd;
+
+   private bool moveCart = false;
+
+   private GameObject cartObject = null;
+   private float progress = 0f; // 0 - 1, basically a percentage
+
+   private void Awake() {
       if (_layerObject == null) _layerObject = GetComponent<LayerGameObjectPlacement>();
    }
 
 
-   private void Start()
-   {
-      foreach (LocationUnit unit in units)
-      {
+   private void Start() {
+      cartStart = new Vector2((float)units[0].position.Latitude, (float)units[0].position.Longitude);
+      cartEnd = new Vector2((float)units[units.Count - 1].position.Latitude, (float)units[units.Count - 1].position.Longitude);
+
+      foreach (LocationUnit unit in units) {
          GameObject location = _layerObject.PlaceInstance(unit.position, unit.name).Value;
          location.GetComponent<SceneLoader>().SceneId = unit.sceneID;
-         if (location.GetComponent<CompletionCheck>() != null) { location.GetComponent<CompletionCheck>().levelId = unit.preRequisiteLevelId; }
+         if (location.GetComponent<CompletionCheck>() != null) {
+            location.GetComponent<CompletionCheck>().levelId = unit.preRequisiteLevelId;
+         }
 
-         if (unit.child != null)
-         {
+         if (unit.child != null) {
             GameObject c = Instantiate(unit.child,location.transform.position, Quaternion.identity);
-            if (c.GetComponent<CompletionCheck>() != null) { c.GetComponent<CompletionCheck>().levelId = unit.levelId; }
+            if (c.GetComponent<CompletionCheck>() != null) {
+               c.GetComponent<CompletionCheck>().levelId = unit.levelId;
+            }
          }
       }
-      
-   }
-    
 
+      Vector2 cartPos = cartStart + ((cartEnd - cartStart) * progress);
+
+      SerializableLatLng cartLatLon = new SerializableLatLng(cartPos.x, cartPos.y);
+
+      cartObject = cart.PlaceInstance(cartLatLon, "HotMetalCartPlacementLayer").Value;
+   }
+
+   private void Update() {
+      if (Input.GetKeyDown(KeyCode.Space)) {
+         if (!moveCart && progress < 1f) {// && cartObject != null) {
+            if (cartObject != null) {
+               Destroy(cartObject);
+            }
+
+            progress += .1f;
+
+            Vector2 cartPos = cartStart + ((cartEnd - cartStart) * progress);
+            SerializableLatLng cartLatLon = new SerializableLatLng(cartPos.x, cartPos.y);
+
+            cartObject = cart.PlaceInstance(cartLatLon, "HotMetalCartPlacementLayer").Value;
+         }
+
+         moveCart = true;
+      } else if (Input.GetKeyUp(KeyCode.Space)) {
+         moveCart = false;
+      }
+   }
 }
