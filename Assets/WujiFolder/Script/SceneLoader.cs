f112e26baddd36ee232aa2c1f040cb4879f270b9
cwj2099 <wc1629@nyu.edu>
2024-04-04 15:47:31 -0400

    bug fix

--- Assets/WujiFolder/Script/SceneLoader.cs
+++ Assets/WujiFolder/Script/SceneLoader.cs
@@ -6,50 +6,64 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SceneLoader : MonoBehaviour {
-   public int SceneId;
+public class SceneLoader : MonoBehaviour
+{
+    public int SceneId;
 
-   AsyncOperation asyncOperation;
-   public void LoadScene() {
+    AsyncOperation asyncOperation;
+    public void LoadScene()
+    {
+        if (asyncOperation != null) return;
 
-      if (Fader.instance != null) {
-         asyncOperation = SceneManager.LoadSceneAsync(SceneId);
-         asyncOperation.allowSceneActivation = false;
+        if (Fader.instance != null)
+        {
+            asyncOperation = SceneManager.LoadSceneAsync(SceneId);
+            asyncOperation.allowSceneActivation = false;
 
-         Fader.instance.loadGroup.gameObject.SetActive(true);
-         Fader.instance.img.DOFade(1, Fader.fadeTime).OnComplete(() => {
-            Transition(() => { asyncOperation.allowSceneActivation = true; });
-         });
-      } else {
-         DOTween.Sequence().AppendInterval(Fader.fadeTime).OnComplete(() => { SceneManager.LoadScene(SceneId); });
-      }
-   }
+            Fader.instance.loadGroup.gameObject.SetActive(true);
+            Fader.instance.img.DOFade(1, Fader.fadeTime).OnComplete(() => {
+                Transition(() => { asyncOperation.allowSceneActivation = true; });
+            });
+        }
+        else
+        {
+            DOTween.Sequence().AppendInterval(Fader.fadeTime).OnComplete(() => { SceneManager.LoadScene(SceneId); });
+        }
+    }
 
-   public void LoadCurrent() {
-      if (Fader.instance != null) {
-         asyncOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
-         asyncOperation.allowSceneActivation = false;
+    public void LoadCurrent()
+    {
+        if (asyncOperation != null) return;
 
-         Fader.instance.loadGroup.gameObject.SetActive(true);
-         Fader.instance.img.DOFade(1, Fader.fadeTime).OnComplete(() => {
-            Transition(() => { asyncOperation.allowSceneActivation = true; });
-         });
-      } else {
-         DOTween.Sequence().AppendInterval(Fader.fadeTime).OnComplete(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().name); });
-      }
-   }
+        if (Fader.instance != null)
+        {
+            asyncOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+            asyncOperation.allowSceneActivation = false;
 
-   private void Update() {
-      if (asyncOperation != null) {
-         Fader.instance.progress.fillAmount = asyncOperation.progress;
-      }
-   }
+            Fader.instance.loadGroup.gameObject.SetActive(true);
+            Fader.instance.img.DOFade(1, Fader.fadeTime).OnComplete(() => {
+                Transition(() => { asyncOperation.allowSceneActivation = true; });
+            });
+        }
+        else
+        {
+            DOTween.Sequence().AppendInterval(Fader.fadeTime).OnComplete(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().name); });
+        }
+    }
 
-   void Transition(UnityAction action) {
-      Fader.instance.loadGroup.DOFade(1, 0.5f).From(0);
-      DOTween.Sequence().AppendInterval(2.5f).
-         Append(Fader.instance.loadGroup.DOFade(0, 0.5f).From(1)).AppendInterval(1f).
-         OnComplete(action.Invoke);
-   }
+    private void Update()
+    {
+        if (asyncOperation != null)
+        {
+            Fader.instance.progress.fillAmount = asyncOperation.progress;
+        }
+    }
+
+    void Transition(UnityAction action)
+    {
+        Fader.instance.loadGroup.DOFade(1, 0.5f).From(0);
+        DOTween.Sequence().AppendInterval(2.5f).
+           Append(Fader.instance.loadGroup.DOFade(0, 0.5f).From(1)).AppendInterval(1f).
+           OnComplete(action.Invoke);
+    }
 }
-//90(54)+96.5(14.5)+5+5
